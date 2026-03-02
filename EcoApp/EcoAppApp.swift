0bfa23e5ba91deed1f433e10f5b514bddfbff361
G0yaveee <A01571291@gmail.com>
2026-03-02 16:16:11 -0600

    Initial Commit

---
+++ EcoApp/EcoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoAppApp.swift
+//  EcoApp
+//
+//  Created by Mar Reyes on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoAppTests/EcoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcoAppTests.swift
+//  EcoAppTests
+//
+//  Created by Mar Reyes on 02/03/26.
+//
+
+import Testing
+@testable import EcoApp
+
+struct EcoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
